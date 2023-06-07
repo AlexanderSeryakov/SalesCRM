2f37813c7a1d2b7dc72431d148576e9cd4917b2d
Alexander Seryakov <seryakov78116@gmail.com>
2023-06-07 13:27:17 +0300

    Added Sale Model. Sales page.

--- sale/models.py
+++ sale/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Sale(models.Model):
+    product_name = models.CharField(max_length=256)
+    quantity = models.IntegerField(default=1)
+    price = models.FloatField(default=0)
+    discount = models.FloatField(default=0, null=True, blank=True)
+    customer_name = models.CharField(max_length=512)
+    customer_phone = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Sale'
+        verbose_name_plural = 'Sales'
+
+    def __str__(self):
+        return self.product_name
+
+    def get_absolute_url(self):
+        return self.pk
+
+    @property
+    def get_total_price(self):
+        total = self.price * self.quantity
+        return total - total * (self.discount / 100)
+
+
+
--- sale/views.py
+++ sale/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from . import models
 
-# Create your views here.
+
+class SalesPageView(ListView):
+    model = models.Sale
+    http_method_names = ['get', 'head']
+    template_name = 'sale/sales.html'
+    context_object_name = 'sales'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Sales'
+        return context
