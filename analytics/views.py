56fa95677b7c5715bfc21dc1749411aba9e32ea9
Alexander Seryakov <seryakov78116@gmail.com>
2023-06-09 16:41:36 +0300

    Minor Edits

--- analytics/views.py
+++ analytics/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 from sale.models import Sale
 
 
 def home(request):
     sales = Sale.objects.filter(user_id=request.user.pk)
-    return HttpResponse(f"Приветствую в разделе аналитики, {request.user} ! Всего сделано: {len(sales)} продаж.")
+    return render(request, 'analytics/base.html', {})
