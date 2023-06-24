b23c24fd16bb6c855f4a58bbd4d9358c4bbfdda4
Alexander Seryakov <seryakov78116@gmail.com>
2023-06-24 18:27:21 +0300

    Refactored analytics app. Added new functionality.

--- apps/analytics/utils.py
+++ apps/analytics/utils.py
@@ -3,7 +3,15 @@ from datetime import datetime
 from apps.sale.models import Sale
 
 
-def get_valid_data_for_template(user_id, time_range=None):
+def get_valid_data_for_template(user_id: int, time_range=None) -> list[dict]:
+    """ Calculates total and net profit for each product,
+    the number of sales and the total number of items sold for the period=time_range.
+    Enters data into a dictionary of the desired type.
+
+    :param user_id: user.pk by which sales are filtered
+    :param time_range: time range for which you need to select sales. Default time_range for today
+    :return: tuple[list[dict], list[float], list[dict], list[dict]]
+    """
     if time_range is None:
         time_range = [datetime.today(), datetime.today()]
 
@@ -29,4 +37,44 @@ def get_valid_data_for_template(user_id, time_range=None):
             data_set[sale.product_id]['total_score_cleaned'] += sale.get_clean_total_score
 
     return list(data_set.values())
+
+
+def get_total_profit_and_tax(data_set: list[dict]) -> list[float]:
+    """
+    Based on the passed list of dictionaries,
+    calculates the total profit from
+    the total cleaned profit and the total tax from all sales.
+
+    Dictionaries must have keys 'total_score' and 'total_score_cleaned'.
+
+    :param data_set: list[dict]
+    :return: tuple(float)
+    :raises KeyError
+    """
+    total_tax, total_profit, total_cleaned_profit = 0, 0, 0
+    for sale in data_set:
+        total_profit += sale['total_score']
+        total_cleaned_profit += sale['total_score_cleaned']
+
+    total_tax = total_profit * 0.06
+
+    return [total_profit, total_cleaned_profit, total_tax]
+
+
+def get_top_products(data_set: list[dict]) -> list[[dict]]:
+    """
+    The function takes a list of dictionaries. Based on it, 2 lists are formed: top_sales, top_profit.
+
+    Dictionaries must have keys 'total_quantity_all' and 'total_score_cleaned'.
+    End values are sorted in descending order
+
+    :param data_set:
+    :return: list with 2 float-values. top 5 products by the numbser of sales
+    and top 5 products by cleaned profit for the period.
+    :raises KeyError
+    """
+    top_sales = sorted(data_set, key=lambda sale: sale['total_quantity_all'], reverse=True)[:5]
+    top_profit = sorted(data_set, key=lambda sale: sale['total_score_cleaned'], reverse=True)[:5]
+
+    return [top_sales, top_profit]
 
--- apps/analytics/views.py
+++ apps/analytics/views.py
@@ -3,20 +3,28 @@ from datetime import datetime
 from django.shortcuts import redirect, render
 from django.views import View
 from .forms import MyDateInput
-from .utils import get_valid_data_for_template
+from .utils import get_valid_data_for_template, get_total_profit_and_tax, get_top_products
 
 
 class AnalyticsView(View):
     template = 'analytics/base.html'
     date_form = MyDateInput()
+    title = 'Statistic'
 
     def get(self, request):
         data_set = get_valid_data_for_template(self.request.user.pk)
+        total_values = get_total_profit_and_tax(data_set)
+        top_sales, top_profit = get_top_products(data_set)
+
         time_range = datetime.today()
+
         return render(request, template_name=self.template, context={'data_set': data_set, 'form': self.date_form,
-                                                                     'title': 'Statistic',
+                                                                     'title': self.title,
                                                                      'time_start': time_range,
                                                                      'time_end': time_range,
+                                                                     'total_values': total_values,
+                                                                     'top_sales': top_sales,
+                                                                     'top_profit': top_profit,
                                                                      })
 
     def post(self, request):
@@ -26,10 +34,17 @@ class AnalyticsView(View):
             return redirect('analytics')
 
         data_set = get_valid_data_for_template(self.request.user.pk, time_range=time_range)
+        total_values = get_total_profit_and_tax(data_set)
+        top_sales, top_profit = get_top_products(data_set)
+
         time_for_template = [datetime.strptime(time_range[0], '%Y-%m-%d'), datetime.strptime(time_range[1], '%Y-%m-%d')]
+
         return render(request, template_name=self.template, context={'data_set': data_set, 'form': self.date_form,
-                                                                     'title': 'Statistic',
+                                                                     'title': self.title,
                                                                      'time_start': time_for_template[0],
                                                                      'time_end': time_for_template[1],
+                                                                     'total_values': total_values,
+                                                                     'top_sales': top_sales,
+                                                                     'top_profit': top_profit,
                                                                      })
 
